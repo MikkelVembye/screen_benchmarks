c24959ddecf35a763a2223d7d4e3faa2043ce9c8
Anja Bondebjerg <B058881@PROD.SITAD.DK>
2023-11-14 17:56:20 +0100

    Anja leger videre

--- Participation in Organised Sport to Improve and Prevent Adverse Developmental Trajectories of At-Risk Youth/ANBO script.R
+++ Participation in Organised Sport to Improve and Prevent Adverse Developmental Trajectories of At-Risk Youth/ANBO script.R
@@ -11,10 +11,20 @@ Screen_on_Title_Abstract_full_coding_report_1_ <- read_csv("Screen on Title & Ab
 View(Screen_on_Title_Abstract_full_coding_report_1_)
 
 
-
 html <- read_html("Screen on Title & Abstract full coding report (1).html")
 
 screen_report_dat_raw <- 
   html |> 
   html_element("table") |> 
   html_table()
+
+# Til Anja: Du kan bruge glimpse() til at få overblik over variable navnene
+screen_report_dat_raw |> glimpse()
+
+# Read duplicate and delete refs 
+
+miss_refs_ids <- readRDS("")
+
+miss_refs_raw <- 
+  screen_report_dat_raw |> 
+  filter(ItemId %in% miss_refs_ids)
