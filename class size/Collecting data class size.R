4047580e9e6e71b942484be1d6dda0881f39d5fa
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-11-20 16:19:17 +0100

    Updating class size review single screen

--- class size/Collecting data class size.R
+++ class size/Collecting data class size.R
@@ -11,36 +11,27 @@ html <- read_html("class size/class size screening on title and abstract full co
 screen_report_dat_raw <- 
   html |> 
   html_element("table") |> 
-  html_table()
+  html_table() |> 
+  filter(str_detect(`I/E/D/S flag`, "(I)|(E)")) 
 
 # Til Anja: Du kan bruge glimpse() til at få overblik over variable navnene
 screen_report_dat_raw |> glimpse()
 
 screen_report_dat <- 
   screen_report_dat_raw |> 
-  filter(str_detect(`I/E/D/S flag`, "(I)|(E)")) |> 
   select(-`I/E/D/S flag`) |> 
   rename(
     eppi_id= ItemId,
     author_short = ShortTitle, 
     title_report = Title, 
     include = Include,
-    exclude1 = Uncertain, 
-    exclude2 = Exclude,
-    exclude3 = `Exclude on Intervention`,
-    exclude4 = `Exclude on participants`,
-    exclude5 = `Exclude on study type`,
-    exclude6 = `Exclude on review`,
-    
+    uncertain = Uncertain, 
+    exclude1 = Exclude,
+    exclude2 = `Exclude on Intervention`,
+    exclude3 = `Exclude on participants`,
+    exclude4 = `Exclude on study type`,
+    exclude5 = `Exclude on review`
   ) |> 
-  mutate(
-    across(include:exclude6, ~ na_if(.x, "")),
-    include = if_else(!is.na(include), "Included", NA_character_),
-    exclude = if_any(exclude1:exclude6, ~ !is.na(.x)),
-    exclude = if_else(exclude == TRUE, "Excluded", NA_character_)
-  ) |> 
-  select(-c(exclude1:exclude6)) |> 
-  relocate(exclude, .before = include) |> 
   arrange(eppi_id) 
 
 # Loading included and excluded studies
@@ -76,13 +67,88 @@ ids <- cl_size_ris_dat |> pull(eppi_id)
 # Not among ris-file references
 missing_stud <- 
   screen_report_dat_raw |> 
-  filter(!ItemId %in% ids & !str_detect(`I/E/D/S flag`, "(S)"))
+  filter(!ItemId %in% ids)
 
 # Removing the 4 references that does not appear among ris-file references
 screen_report_dat_filtered <- 
   screen_report_dat |> 
   filter(eppi_id %in% ids)
 
+cl_size_dat_wide <- 
+  left_join(screen_report_dat_filtered, cl_size_ris_dat, by = join_by(eppi_id)) |> 
+  arrange(final_human_decision) |> 
+  filter(uncertain == "") |>  # Removing uncertain decisions
+  select(-uncertain)
+  
+# Detecting individual screener names
+screeners_var <- 
+  screen_report_dat_filtered |> 
+  reframe(
+    name = unique(c_across(exclude1:include))
+  ) |> 
+  pull(name)
+
+screeners <- 
+  unlist(strsplit(screeners_var, "(?<=[a-z])(?=[A-Z])", perl = TRUE)) |> 
+  gsub("\\[.*","", x = _) |> 
+  unique() 
+
+
+filter_list <- list()
+
+for (i in 1:length(screeners)){
+  filter_list[[i]] <- 
+    cl_size_dat_wide |> 
+    filter(if_any(include:exclude5, ~ str_detect(.x, screeners[i]))) |> 
+    mutate(
+      screener = screeners[i],
+      across(include:exclude5, ~ str_extract(.x, screeners[i]))
+    ) |> 
+    relocate(screener)
+}
+
+single_screen_dat <- 
+  filter_list |> 
+  list_rbind() |> 
+  mutate(
+    exclude = if_any(exclude1:exclude5, ~ !is.na(.x)),
+    exclude = if_else(exclude == TRUE, screener, NA_character_),
+    screener_decision = case_when(
+      !is.na(include) ~ 1,
+      !is.na(exclude) ~ 0,
+      !is.na(include) & !is.na(exclude) ~ 2, # Indicating non decisive answer
+      TRUE ~ NA_real_
+    ),
+    conflict = if_else(screener_decision != final_human_decision, 1, 0)
+  ) |> 
+  select(-c(exclude1:exclude5)) |> 
+  relocate(exclude, .before = include) |> 
+  relocate(screener_decision, .before = final_human_decision)
+
+cl_size_single_perform_dat <- 
+  single_screen_dat |> 
+  summarise(
+    TP = sum(screener_decision == 1 & final_human_decision == 1),
+    TN = sum(screener_decision == 0 & final_human_decision == 0),
+    FN = sum(screener_decision == 0 & final_human_decision == 1),
+    FP = sum(screener_decision == 1 & final_human_decision == 0),
+    recall = TP / (TP + FN),
+    spec = TN / (TN + FP),
+    bacc = (recall + spec) / 2,
+    .by = screener
+  ) |> 
+  mutate(
+    review_authors = "Filges, et al. (2018)",
+    review = "Class size",
+    role = rep(c("Assistant", "Author"), c(5,1)),
+  ) |> 
+  relocate(review_authors:role)
+
+saveRDS(cl_size_single_perform_dat, "single screener data/cl_size_single_perform_dat.rds")
+
+#------------------------------------------------------------------------------
+# Old
+#------------------------------------------------------------------------------
 cl_size_dat <- 
   left_join(screen_report_dat_filtered, cl_size_ris_dat, by = join_by(eppi_id)) |> 
   select(-c(author_short, title_report)) |> 
