3820c90b7a8f70cf55c6207aad48fb01d95203a7
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-11-20 16:15:14 +0100

    Updating 12-step

--- 12-step/create 12 step data.R
+++ 12-step/create 12 step data.R
@@ -11,18 +11,19 @@ html <- read_html("12-step/12step screen on title & abstract full coding report.
 screen_report_dat_raw <- 
   html |> 
   html_element("table") |> 
-  html_table()
+  html_table() |> 
+  filter(str_detect(`I/E/D/S flag`, "(I)|(E)"))
 
 # Til Anja: Du kan bruge glimpse() til at få overblik over variable navnene
 screen_report_dat_raw |> glimpse()
 
 
 # Read duplicate and delete refs (Tal med Anja)
-miss_refs_ids <- readRDS("12-step/duplicate_delete_ids.rds")
-
-miss_refs_raw <- 
-  screen_report_dat_raw |> 
-  filter(ItemId %in% miss_refs_ids)
+#miss_refs_ids <- readRDS("12-step/duplicate_delete_ids.rds")
+#
+#miss_refs_raw <- 
+#  screen_report_dat_raw |> 
+#  filter(ItemId %in% miss_refs_ids)
 
 # All studies coded by Martin Boeg was single screened. 
 # Therefore, we exclude these references
@@ -44,7 +45,7 @@ boeg_refs <-
     across(exclude1:include, ~ str_replace_all(.x, "BÃ¸g", "Boeg"))
   ) |> 
   arrange(eppi_id) |> 
-  filter(eppi_id != "FullPath:") |>  # removing first row
+  #filter(eppi_id != "FullPath:") |>  # removing first row
   filter(if_any(exclude1:include, ~ str_detect(., "Boeg"))) |> 
   pull(eppi_id)
 
@@ -63,12 +64,9 @@ screen_report_dat <-
     include = `Include on basis of title and abstract`
   ) |> 
   mutate(
-    across(exclude1:include, ~ na_if(.x, "")),
     across(exclude1:include, ~ str_replace_all(.x, " BÃ¸g", "Boeg"))
   ) |> 
-  arrange(eppi_id) |> 
-  filter(eppi_id != "FullPath:") 
-
+  arrange(eppi_id) 
 
 # Loading included and excluded studies
 ex_paths <- list.files("12-step/", pattern = "excl")
@@ -97,33 +95,81 @@ step12_incl <-
 
 step12_ris_dat <- bind_rows(step12_excl, step12_incl) 
 
-# ids til filter screen report data
-ids <- step12_ris_dat |> pull(eppi_id)
 
-# FIND DUPLICATE AND DELETE FLAGED STUDIES
-#miss_refs <- 
-#  screen_report_dat |> 
-#  filter(!eppi_id %in% ids) 
-#
-#miss_refs_ids <- 
-#  screen_report_dat |> 
-#  filter(!eppi_id %in% ids) |> 
-#  pull(eppi_id)
-#
-#saveRDS(miss_refs_ids, file = "12-step/duplicate_delete_ids.rds")
+step12_dat_wide <- 
+  left_join(screen_report_dat, step12_ris_dat, by = join_by(eppi_id)) |> 
+  filter(!eppi_id %in% boeg_refs) |> 
+  arrange(final_human_decision)
 
-# filter screen report data
-screen_report_dat_filtered <- 
-  screen_report_dat |> 
-  # removing duplicates and delete flaged studies + single-screened studies
-  filter(eppi_id %in% ids & if_any(exclude1:include, ~ str_detect(., "Boeg", negate = TRUE))) |> 
+# Detecting individual screener names
+screeners_var <- 
+  step12_dat_wide |> 
+  reframe(
+    name = unique(c_across(exclude1:include))
+  ) |> 
+  pull(name)
+
+screeners <- 
+  unlist(strsplit(screeners_var, "(?<=[a-z])(?=[A-Z])", perl = TRUE)) |> 
+  gsub("\\[.*","", x = _) |> 
+  unique() 
+
+filter_list <- list()
+
+for (i in 1:length(screeners)){
+  filter_list[[i]] <- 
+    step12_dat_wide |> 
+    filter(if_any(exclude1:include, ~ str_detect(.x, screeners[i]))) |> 
+    mutate(
+      screener = screeners[i],
+      across(exclude1:include, ~ str_extract(.x, screeners[i]))
+    ) |> 
+    relocate(screener)
+}
+
+single_screen_dat <- 
+  filter_list |> 
+  list_rbind() |> 
   mutate(
-    include = if_else(!is.na(include), "Included", NA_character_),
     exclude = if_any(exclude1:exclude4, ~ !is.na(.x)),
-    exclude = if_else(exclude == TRUE, "Excluded", NA_character_)
+    exclude = if_else(exclude == TRUE, screener, NA_character_),
+    screener_decision = case_when(
+      !is.na(include) ~ 1,
+      !is.na(exclude) ~ 0,
+      !is.na(include) & !is.na(exclude) ~ 2, # Indicating non decisive answer
+      TRUE ~ NA_real_
+    ),
+    conflict = if_else(screener_decision != final_human_decision, 1, 0)
   ) |> 
   select(-c(exclude1:exclude4)) |> 
-  relocate(exclude, .before = include)
+  relocate(exclude, .before = include) |> 
+  relocate(screener_decision, .before = final_human_decision)
+
+step12_single_perform_dat <- 
+  single_screen_dat |> 
+  filter(screener %in% screeners[1:2]) |> 
+  summarise(
+    TP = sum(screener_decision == 1 & final_human_decision == 1),
+    TN = sum(screener_decision == 0 & final_human_decision == 0),
+    FN = sum(screener_decision == 0 & final_human_decision == 1),
+    FP = sum(screener_decision == 1 & final_human_decision == 0),
+    recall = TP / (TP + FN),
+    spec = TN / (TN + FP),
+    bacc = (recall + spec) / 2,
+    .by = screener
+  ) |> 
+  mutate(
+    review_authors = "Bøg, Filges et al. (2017)",
+    review = "12-step programs",
+    role = c("Author", "Assistant"),
+  ) |> 
+  relocate(review_authors:role)
+
+
+#------------------------------------------------------------------------------
+# Old
+#------------------------------------------------------------------------------
+
 
 step12_dat <- 
   left_join(screen_report_dat_filtered, step12_ris_dat, by = join_by(eppi_id)) |> 
