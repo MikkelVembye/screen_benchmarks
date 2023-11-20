0e4d6afbdb2ae416261e2f596c3100ec934b812a
ABondebjerg <150683545+ABondebjerg@users.noreply.github.com>
2023-11-20 14:22:59 +0100

    Add files via upload
    All files needed for adult child ratio

---
+++ Adult child ratio/basis script.R
@@ -0,0 +1,22 @@
+# Creating data for adult child ratio 
+library(tidyverse)
+library(rvest)
+library(revtools)
+library(purrr)
+library(openxlsx2)
+library(stringr)
+library(readr)
+
+Screen_on_title_abstract <- read_csv("Screen on Title & Abstract full coding report (1).html")
+View(Screen_on_title_abstract)
+
+
+html <- read_html("Screen on Title & Abstract full coding report (1).html")
+
+screen_report_dat_raw <- 
+  html |> 
+  html_element("table") |> 
+  html_table() |> 
+  filter(str_detect(`I/E/D/S flag`, "(I)|(E)"))
+
+
